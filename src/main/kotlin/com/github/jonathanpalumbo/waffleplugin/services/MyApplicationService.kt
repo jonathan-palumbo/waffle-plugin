2becdf0b9287246c107056b2c107942c9429f0bf
GitHub Action <action@github.com>
2021-06-08 23:53:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jonathanpalumbo/waffleplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jonathanpalumbo.waffleplugin.services
+
+import com.github.jonathanpalumbo.waffleplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/jonathanpalumbo/waffleplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jonathanpalumbo/waffleplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonathanpalumbo.waffleplugin.services
 
+import com.github.jonathanpalumbo.waffleplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
